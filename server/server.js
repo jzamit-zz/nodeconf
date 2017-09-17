5b4aa220e53b858cf05e2de08b51b2a6168b83ab
Jorge Zamit <jzamit@gmail.com>
2017-09-16 22:06:10 -0300

    Added server side socket io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public/src');
 
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
 
+//Websockets server
+let io = socketIO(server);
+
+//Serving public files (Frontend) 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+server.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
+	console.log(publicPath);
 });
