031078f8c9ffce38e6b7b3113324c26d66383697
Jorge Zamit <jzamit@gmail.com>
2017-09-17 11:22:07 -0300

    Refactored arrow function to vanilla js format

--- public/src/js/js.js
+++ public/src/js/js.js
@@ -1,1 +1,9 @@
 var socket = io();
+
+socket.on('connect', function(){
+	console.log('Connected to server...');
+});
+
+socket.on('disconnect', function(){
+	console.log('Disconnected from server...');
+});
